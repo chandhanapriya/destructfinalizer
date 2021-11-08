1dc64ef5dc8985248b6afeb0d6f9f6edf785ccab
chandhanapriya T <92292189+chandhanapriya@users.noreply.github.com>
2021-11-08 19:15:56 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_destruct_finalizer
+{
+    public class Class1:IDisposable
+    {
+      public void Displaymsg()
+        {
+            Console.WriteLine("inside display method");
+          
+        }
+
+        public Class1()
+        {
+            Console.WriteLine("default constructor");
+        }
+
+        public Class1(string msg)
+        {
+            Console.WriteLine("parameterized constructor");
+        }
+        ~Class1()//destructor
+
+        {
+            Console.WriteLine("freeup space");
+        }
+           public void Dispose()
+        {
+            Console.WriteLine("freeup using dispose");
+        
+          
+        }
+    }
+
+ }
+
+
+   
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Demo_destruct_finalizer
+{
+    class Program:Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("finalizer!");
+
+            Class1 obj1 = new Class1();
+            obj1.Displaymsg();
+
+            Class1 obj2 = new Class1("secret message");
+             obj1.Dispose();
+      
+        }
+    }
+}
